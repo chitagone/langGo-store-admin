8fd6f9cc8ac4f9a102cc5052a807ddfacbce9af8
Chittagone <chitagobego@gmail.com>
2024-09-09 17:52:16 +0700

    add navbar setting {delete and update}

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,11 @@
-import Navbar from "@/components/Navbar";
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb"; // Ensure you're importing prismadb correctly
 
-export default async function DashboardLayout({
+export default async function SetupLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { storeId: string };
 }) {
   const { userId } = auth();
 
@@ -18,19 +15,13 @@ export default async function DashboardLayout({
 
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId,
       userId,
     },
   });
 
-  if (!store) {
-    redirect("/");
+  if (store) {
+    redirect(`/${store.id}`);
   }
 
-  return (
-    <>
-      <Navbar />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
