3fd3e2b1b687cd4682127d7a5bb17095afece047
Chittagone <chitagobego@gmail.com>
2024-09-12 19:13:37 +0700

    remove eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  eslint: {
+    // Ignore ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
