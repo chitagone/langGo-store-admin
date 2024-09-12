a75d07da2c036eebb85a30f23ca4c462a7bbb462
Chittagone <chitagobego@gmail.com>
2024-09-13 00:14:28 +0700

    pisma

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  let prisma: PrismaClient | undefined;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
-
-export default prismadb;
+export default prisma;
