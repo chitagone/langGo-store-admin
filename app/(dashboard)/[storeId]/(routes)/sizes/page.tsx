52db6cec0159235d2b49aede671f50dac211d294
Chittagone <chitagobego@gmail.com>
2024-09-13 01:02:07 +0700

    size

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -2,12 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { SizesClient } from "./components/client";
 import { SizesColumn } from "./components/columns";
 import { format } from "date-fns";
-interface Item {
-  id: string;
-  name: string;
-  value: string;
-  createdAt: string;
-}
+
 const SizesPage = async ({ params }: { params: { storeId: string } }) => {
   const sizes = await prismadb.size.findMany({
     where: {
@@ -17,7 +12,7 @@ const SizesPage = async ({ params }: { params: { storeId: string } }) => {
       createdAt: "desc",
     },
   });
-  const formattedSizes: SizesColumn[] = sizes.map((item: Item) => ({
+  const formattedSizes: SizesColumn[] = sizes.map((item) => ({
     id: item.id,
     name: item.name,
     value: item.value,
