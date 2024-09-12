3a549ba0ca40284566103aefaaa14ce65dbbb604
Chittagone <chitagobego@gmail.com>
2024-09-12 19:03:49 +0700

    fix the billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -3,6 +3,12 @@ import { BillboardClient } from "./components/client";
 import { BillboardColumn } from "./components/columns";
 import { format } from "date-fns";
 
+interface Item {
+  id: string;
+  label: string;
+  createdAt: string; // Assuming `createdAt` is a formatted date string
+}
+
 const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
   const billboards = await prismadb.billboard.findMany({
     where: {
@@ -12,11 +18,13 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
       createdAt: "desc",
     },
   });
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-    id: item.id,
-    label: item.label,
-    createAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (item: Item) => ({
+      id: item.id,
+      label: item.label,
+      createAt: format(item.createdAt, "MMMM do, yyyy"),
+    })
+  );
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6 ">
